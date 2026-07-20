2c2fd5ab27be4d8c0f07e012101496558a621c01
yeonghwa <yhkiiim@gmail.com>
2026-07-20 22:00:54 +0900

    photo

--- script.js
+++ script.js
@@ -71,6 +71,9 @@ const GALLERY_PHOTOS = [
   'images/photo1.jpg', 'images/photo2.jpg', 'images/photo3.jpg',
   'images/photo4.jpg', 'images/photo5.jpg', 'images/photo6.jpg',
   'images/photo7.jpg', 'images/photo8.jpg', 'images/photo9.jpg',
+  'images/photo10.jpg', 'images/photo11.jpg', 'images/photo12.jpg',
+  'images/photo13.jpg', 'images/photo14.jpg', 'images/photo15.jpg',
+  'images/photo16.jpg', 'images/photo17.jpg', 'images/photo18.jpg',
 ];
 let modalIndex = 0;
 
